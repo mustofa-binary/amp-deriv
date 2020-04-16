fec0b6e1a8f3824a551eac4098f56e2ac4845073
cakasuma <amammustofa@gmail.com>
2020-04-16 18:48:40 +0800

    header

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,9 @@ import { AnchorLink } from 'gatsby-plugin-anchor-links'
 import Container from 'components/container'
 import Flex from 'components/flex'
 import { Text } from 'components/typography'
+import device from 'themes/device'
 import Deriv from 'assets/svg/header-logo.svg'
+import Hamburger from 'assets/svg/hamburger-menu.svg'
 
 const HeaderWrapper = styled.header`
     position: fixed;
@@ -14,6 +16,15 @@ const HeaderWrapper = styled.header`
     z-index: 100;
     padding: 2.4rem 0;
     box-shadow: 0 5px 15px 0px rgba(0, 0, 0, 0.8);
+`
+
+const HamburgerMenu = styled.button`
+    cursor: pointer;
+    display: none;
+    @media ${device.tabletL} {
+        display: block;
+        cursor: pointer;
+    }
 `
 
 const LinkText = styled(AnchorLink)`
@@ -50,29 +61,62 @@ const BinaryLink = styled.a`
     }
 `
 
+const LinkWrapper = styled(Flex)`
+    @media ${device.tabletL} {
+        display: none;
+    }
+`
+
 const Header = () => (
-    <HeaderWrapper>
-        <Container justifyContent="space-between">
-            <Flex alignItems="center" justifyContent="center">
-                <AnchorLink to="/">
-                    <amp-img src={Deriv} width="212" height="27" />
-                </AnchorLink>
-                <Binary>
-                    A{' '}
-                    <BinaryLink href="https://binary.com" target="_blank" rel="noopener noreferrer">
-                        Binary.com
-                    </BinaryLink>{' '}
-                    brand
-                </Binary>
-            </Flex>
-            <Flex alignItems="center" justifyContent="center">
-                <LinkText to="/#trade">Trade</LinkText>
-                <LinkText to="/#markets">Markets</LinkText>
-                <LinkText to="/#simple-steps">Tutorial</LinkText>
-                <LinkText to="/#our-clients">Testimonials</LinkText>
-            </Flex>
-        </Container>
-    </HeaderWrapper>
+    <>
+        <HeaderWrapper>
+            <Container justifyContent="space-between">
+                <Flex alignItems="center" justifyContent="center">
+                    <AnchorLink to="/">
+                        <amp-img src={Deriv} width="212" height="27" />
+                    </AnchorLink>
+                    <Binary>
+                        A{' '}
+                        <BinaryLink
+                            href="https://binary.com"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Binary.com
+                        </BinaryLink>{' '}
+                        brand
+                    </Binary>
+                </Flex>
+                <LinkWrapper alignItems="center" justifyContent="center">
+                    <LinkText to="/#trade">Trade</LinkText>
+                    <LinkText to="/#markets">Markets</LinkText>
+                    <LinkText to="/#simple-steps">Tutorial</LinkText>
+                    <LinkText to="/#our-clients">Testimonials</LinkText>
+                </LinkWrapper>
+                <HamburgerMenu on="tap:sidebar1.toggle">
+                    <amp-img src={Hamburger} width="18" height="16" />
+                </HamburgerMenu>
+            </Container>
+        </HeaderWrapper>
+        <amp-sidebar id="sidebar1" side="left">
+            <ul>
+                <li>Nav item 1</li>
+                <li>
+                    <a href="#idTwo" on="tap:idTwo.scrollTo">
+                        Nav item 2
+                    </a>
+                </li>
+                <li>Nav item 3</li>
+                <li>
+                    <a href="#idFour" on="tap:idFour.scrollTo">
+                        Nav item 4
+                    </a>
+                </li>
+                <li>Nav item 5</li>
+                <li>Nav item 6</li>
+            </ul>
+        </amp-sidebar>
+    </>
 )
 
 export default Header
